09530e7bc9e2f31f1eea6684cfe7ffa1d5afcb0b
Shelda <shelda.kristie@gmail.com>
2017-03-15 16:01:24 +1100

    fix the bug

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -10,6 +10,9 @@ def is_odd(a_number):
     Look into modulo division using the '%' operator as one way of doing this.
     """
     pass
+    print('is_odd')
+        else:
+    print('is_even')
 
 
 def fix_it(moves=True, should_move=True):
