fa2db48583f3d238cdd66f8b0f769855083c9327
Shelda <shelda.kristie@gmail.com>
2017-03-25 00:06:16 +1100

    made exercise 3 more robust in the feedback, but haven'r used super_asker and not_number_rejector

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -28,7 +28,51 @@ def advancedGuessingGame():
     Remember to think modular. Try to keep your functions small and single
     purpose if you can!
     """
-    pass
+    print("\nwelcome to the guessing game!")
+    while True:
+        try:
+            lowerBound = int(raw_input("Enter a lower bound: "))
+            print("{} is a number. Proceed.".format(lowerBound))
+            break
+        except Exception as e:
+            print("An actual number please({})".format(e))
+
+    while True:
+        try:
+            upperBound = int(raw_input("Enter a upper bound: "))
+            if upperBound > lowerBound:
+                print("{} is a number. Proceed.".format(upperBound))
+                break
+            else:
+                print("Please choose a number over {}".format(lowerBound))
+        except Exception as i:
+            print("An actual number please({})".format(i))
+
+    print("OK then, a number between {} and {} ?".format(lowerBound,
+                                                         upperBound))
+
+    actualNumber = random.randint(lowerBound, upperBound)
+
+    guessed = False
+
+    while not guessed:
+        try:
+            guessedNumber = int(raw_input("guess a number: "))
+            if lowerBound < guessedNumber < upperBound:
+                print("you guessed {},".format(guessedNumber))
+            else:
+                print("a number between {} and {} plez".format(lowerBound,
+                                                               upperBound))
+            if guessedNumber == actualNumber:
+                print("you got it!! It was {}".format(actualNumber))
+                guessed = True
+            elif guessedNumber < actualNumber:
+                print("too small, try again")
+            else:
+                print("too big, try again")
+        except Exception as j:
+            print("An actual number please({})".format(j))
+    return "You got it!"
 
 
 if __name__ == "__main__":
