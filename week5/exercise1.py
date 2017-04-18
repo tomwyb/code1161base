82bcb2e080b0e8f7f62789e179c6e574c450171b
Shelda <shelda.kristie@gmail.com>
2017-04-18 15:27:49 +1000

    updated the file cause i messed it up somehow

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -58,8 +58,6 @@ def do_bunch_of_bad_things():
     print(yet_another_hyp)
 
 
-# return a list of countdown messages, much like in the bad function above.
-# It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
     for i in range(start, (stop)-1, -1):
         print(message + str(i))
@@ -67,6 +65,70 @@ def countdown(message, start, stop, completion_message):
 
 
 # TRIANGLES
+
+# This should be a series of functions that are ultimatly used by
+# triangle_master
+# It should eventually return a dictionary of triangle facts. It should
+# optionally print information as a nicely formatted string. Make printing
+# turned off by default but turned on with an optional argument.
+# The stub functions are made for you, and each one is tested, so this should
+# hand hold quite nicely.
+def calculate_hypotenuse(base, height):
+    pass
+
+
+def calculate_area(base, height):
+    pass
+
+
+def calculate_perimeter(base, height):
+    pass
+
+
+def calculate_aspect(base, height):
+    pass
+
+
+# Make sure you reuse the functions you've already got
+# Don't reinvent the wheel
+def get_triangle_facts(base, height, units="mm"):
+    return {"area": None,
+            "perimeter": None,
+            "height": None,
+            "base": None,
+            "hypotenuse": None,
+            "aspect": None,
+            "units": None}
+
+
+# this should return a multi line string that looks a bit like this:
+#
+# 15
+# |
+# |     |\
+# |____>| \  17.0
+#       |  \
+#       |   \
+#       ------
+#       8
+# This triangle is 60.0mm²
+# It has a perimeter of 40.0mm
+# This is a tall triangle.
+#
+# but with the values and shape that relate to the specific
+# triangle we care about.
+def tell_me_about_this_right_triangle(facts_dictionary):
+    tall = """
+            {height}
+            |
+            |     |\\
+            |____>| \\  {hypotenuse}
+                  |  \\
+                  |   \\
+                  ------
+                  {base}"""
+    wide = """
+            {hypotenuse}
              ↓         ∕ |
                    ∕     | <-{height}
                ∕         |
@@ -87,18 +149,29 @@ def countdown(message, start, stop, completion_message):
     facts = pattern.format(**facts_dictionary)
 
 
+
 def triangle_master(base,
-                    height,
-                    return_diagram=False,
-                    return_dictionary=False):
-    if return_diagram and return_dictionary:
-        return None
-    elif return_diagram:
-        return None
-    elif return_dictionary:
-        return None
-    else:
-        print("You're an odd one, you don't want anything!")
+                    height):
+                    # return_diagram=False,
+                    # return_dictionary=False):
+    # if return_diagram and return_dictionary:
+    #     return None
+    # elif return_diagram:
+    #     return None
+    # elif return_dictionary:
+    #     return None
+    # else:
+    #     print("You're an odd one, you don't want anything!")
+
+    hypotenuse = base**2 + height**2
+    printout = ("This triangle is {area}{units}²\n"
+                "It has a perimeter of {perimeter}{units}\n"
+                "This is a {aspect} triangle.\n")
+    facts = printout.format(area=(base * height)/2,
+                            units="cm",
+                            perimeter=(base + height + hypotenuse),
+                            aspect="")
+    print(facts)
 
 
 def wordy_pyramid():
@@ -119,17 +192,21 @@ def wordy_pyramid():
 
 
 def get_a_word_of_length_n(length):
-    url = baseURL + str(i)
-    r = requests.get(url)
-    message = r.text
+    # url = baseURL + str(i)
+    # r = requests.get(url)
+    # message = r.text
+    pass
 
 
 def list_of_words_with_lengths(start, end, stop):
-    for i in range(start, end, stop)
-    get_a_word_of_length_n
+    pass
+
 
 if __name__ == "__main__":
     do_bunch_of_bad_things()
-    # countdown()
     print(countdown("Getting ready to start in ", 9, 1, "Let's go!"))
+    print(triangle_master(3,
+                          4))
+    #   return_diagram=False,
+    #   return_dictionary=False))
     # wordy_pyramid()
